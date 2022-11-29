7c194cec37849647636a61a2290754296ec82705
e277 <ezramuir12@gmail.com>
2022-11-29 09:42:31 -0500

    Testing out the having keyword

---
+++ SQL-Server-Sample-Database/10. Having.sql
@@ -0,0 +1,13 @@
+SELECT
+    customer_id,
+    YEAR (order_date) order_year,
+    COUNT (order_id) order_count
+FROM
+    sales.orders
+GROUP BY
+    customer_id,
+    YEAR (order_date)
+HAVING
+    COUNT (order_id) >= 2
+ORDER BY
+    customer_id;
